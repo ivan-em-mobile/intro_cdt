efc62c09308635dfc8f5bccbb09e6d121ecd81c6
Ivan Paulino <ivan.paulino@vocacao.org.br>
2026-03-08 21:42:59 -0300

    update-intro_cdt

---
+++ intro_cdt-old/mensagem_boas_vindas.py
@@ -0,0 +1,33 @@
+'''
+Primicias
+Pedir o nome e idade;
+armazenar em str, armazenar em int
+Exibe em tela a mensagem
+concatenados com nome e idade
+'''
+
+print("\n******************")
+print("\fBem vindo a CDT!")
+print("\n******************")
+
+nome_pessoa = str(input
+    ("Qual é o seu Nome? ")
+    )
+
+# variavel_forma = type(input("a pergunta"))
+
+idade_pessoa = int(input
+    ("Qual é sua idade ?")
+    )
+
+altura_pessoa = float(input
+    ("Qual é sua altura? ")
+)
+
+print(f"Olá, {nome_pessoa} a sua idade é {idade_pessoa} e voce diz que tem, {altura_pessoa}")
+
+#python 1.14
+# nome_pessoa = input("Qual é o seu Nome? ")
+#str = nome_pessoa
+
+#python 3.14
---
+++ intro_cdt-old/renan_test.py
@@ -0,0 +1,8 @@
+idade = input ("qual a sua idade?") #int
+demaior = False #bool
+idade>18 demaior = True
+idade<18 demaior = False
+
+demaior = False(f"Sua idade étem que ser maior que 18, sua idade é {idade}")
+
+idade<18 (f"sua ídade é{idade}, pode proceguir")
